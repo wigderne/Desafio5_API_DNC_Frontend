9ab0a4be888d9ad8ebc64705ab2bd43e9b64fc06
Wigder Monteiro Neto <wigderne@gmail.com>
2024-04-06 14:41:17 -0300

    first commit

---
+++ src/api/LivrosService.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3000"
+//const BASE_URL = "http://localhost:8080";
+
+export class LivrosService{
+    static getLivros(){
+        return axios.get(BASE_URL+'/livros');
+    }
+
+    static getLivro(id){
+        return axios.get(`${BASE_URL}/livros/${id}`);
+    }
+
+    static createLivro(body){
+        return axios.post(`${BASE_URL}/livros`,body);
+    }
+
+    static updateLivro(id,body){
+        return axios.put(`${BASE_URL}/livros/${id}`,body);
+    }
+
+    static deleteLivro(id){
+        return axios.delete(`${BASE_URL}/livros/${id}`);
+    }
+    
+}
